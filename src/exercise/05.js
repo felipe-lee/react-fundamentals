0219bdf3b1d5bcac9269667310ac499ccd1b5cac
Felipe Lee <felipe@truss.works>
2021-03-09 18:23:07 -0600

    Create custom Box component to encapsulate repeated logic

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,47 +4,29 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 💰 Use the className for the size and style (backgroundColor) for the color
-// 💰 each of the elements should also have the "box" className applied
-
-// 🐨 add a className prop to each of these and apply the correct class names
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-
-// 🐨 add a style prop to each of them as well so their background color
-// matches what the text says it should be as well as `fontStyle: 'italic'`
-const sharedStyle = {fontStyle: 'italic'}
-
-const smallBox = (
-  <div
-    className="box box--small"
-    style={{...sharedStyle, backgroundColor: 'lightblue'}}
-  >
-    small lightblue box
-  </div>
-)
-const mediumBox = (
-  <div
-    className="box box--medium"
-    style={{...sharedStyle, backgroundColor: 'pink'}}
-  >
-    medium pink box
-  </div>
-)
-const largeBox = (
-  <div
-    className="box box--large"
-    style={{...sharedStyle, backgroundColor: 'orange'}}
-  >
-    large orange box
-  </div>
-)
+const Box = ({className = '', style, ...otherProps}) => {
+  return (
+    <div
+      className={`box ${className}`.trim()}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+      <Box>Things</Box>
     </div>
   )
 }
