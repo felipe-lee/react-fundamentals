22b283125fe6e4ccecc1f22e7b30aabede071434
Felipe Lee <felipe@truss.works>
2021-03-15 15:52:18 -0500

    Add solution for form input using a ref

---
+++ src/exercise/06.extra-1.js
@@ -0,0 +1,31 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const inputRef = React.useRef()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    onSubmitUsername(inputRef.current.value)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor={'usernameInput'}>Username:</label>
+        <input id="usernameInput" type="text" ref={inputRef} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
