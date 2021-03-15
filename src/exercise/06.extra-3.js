4c4516d04359de42ba5765fcc6f8a4a544b3ae5f
Felipe Lee <felipe@truss.works>
2021-03-15 16:39:59 -0500

    Add solution to forms extra credit 3 - controlling input values

---
+++ src/exercise/06.extra-3.js
@@ -0,0 +1,40 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [username, setUsername] = React.useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    onSubmitUsername(username)
+  }
+
+  const handleChange = event => {
+    setUsername(event.target.value.toLowerCase())
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor={'usernameInput'}>Username:</label>
+        <input
+          id="usernameInput"
+          onChange={handleChange}
+          type="text"
+          value={username}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
